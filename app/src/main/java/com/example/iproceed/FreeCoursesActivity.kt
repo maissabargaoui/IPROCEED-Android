397e7d91c4e1cd1c5b0316c553b2dd75d640fa9f
mayssabargaoui1 <mayssabargaoui@esprit.tn>
2022-04-12 02:42:48 +0100

    Sign Up UI

---
+++ app/src/main/java/com/example/iproceed/FreeCoursesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.iproceed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FreeCoursesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_free_courses)
+    }
+}
